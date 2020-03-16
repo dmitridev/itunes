a3c2d28f3a2622ec23f6c148019a91a7a8d39f78
Vast <svoutjekk@gmail.com>
2020-03-16 22:13:42 +0300

    UPD: stable

--- src/main/java/org/astelit/itunes/contstraints/Cyrillic.java
+++ src/main/java/org/astelit/itunes/contstraints/Cyrillic.java
@@ -12,7 +12,7 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @NotEmpty
-@Pattern(regexp = "^[а-яА-Я]*$", message = "содержит недопустимые символы")
+@Pattern(regexp = "^[а-яА-Я]*$", message = "должен состоять из русских букв")
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RUNTIME)
 @Documented
